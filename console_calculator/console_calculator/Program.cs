4f430f2591852a7ad71c80cc6007f81ad9d8772a
calvin3919 <calvin3919@gmail.com>
2022-06-12 23:32:24 -0700

    added comments

--- console_calculator/console_calculator/Program.cs
+++ console_calculator/console_calculator/Program.cs
@@ -4,7 +4,7 @@ namespace Calculator
 {
     class Calculator
     {
-        public static double DoOperation(double num1, double num2, string op)
+        public static double DoOperation(double num1, double num2, string op) //계산하는 클래스
         {
             double result = double.NaN; // Default value is "not-a-number" which we use if an operation, such as division, could result in an error.
 
@@ -35,23 +35,25 @@ namespace Calculator
         }
     }
 
-    class Program
+    class Program //직접적인 프로그램
     {
         static void Main(string[] args)
         {
-            bool endApp = false;
+            bool endApp = false; //앱이 끝나는게 False = 시작한다는 의미
+
             // Display title as the C# console calculator app.
-            Console.WriteLine("Console Calculator in C#\r");
+            Console.WriteLine("Console Calculator in C#\r"); //이문장부터 시작
             Console.WriteLine("------------------------\n");
 
-            while (!endApp)
+            while (!endApp) //반복문이 시작되는 구간 
             {
                 // Declare variables and set to empty.
-                string numInput1 = "";
+                string numInput1 = ""; //문자열 넘버1을 ""넣는다                                       
                 string numInput2 = "";
-                double result = 0;
+                double result = 0; 
 
-                // Ask the user to type the first number.
+                // 처음 숫자를 입력받는다
+
                 Console.Write("Type a number, and then press Enter: ");
                 numInput1 = Console.ReadLine();
 
@@ -62,7 +64,8 @@ namespace Calculator
                     numInput1 = Console.ReadLine();
                 }
 
-                // Ask the user to type the second number.
+                // 두번째 숫자를 입력받는다
+
                 Console.Write("Type another number, and then press Enter: ");
                 numInput2 = Console.ReadLine();
 
@@ -73,7 +76,8 @@ namespace Calculator
                     numInput2 = Console.ReadLine();
                 }
 
-                // Ask the user to choose an operator.
+                // 계산방법 옵션
+
                 Console.WriteLine("Choose an operator from the following list:");
                 Console.WriteLine("\ta - Add");
                 Console.WriteLine("\ts - Subtract");
@@ -83,16 +87,18 @@ namespace Calculator
 
                 string op = Console.ReadLine();
 
+                
                 try
                 {
-                    result = Calculator.DoOperation(cleanNum1, cleanNum2, op);
+                    result = Calculator.DoOperation(cleanNum1, cleanNum2, op);//계산기 다른 클래스에서 각각 받은 값을 출력
+
                     if (double.IsNaN(result))
                     {
                         Console.WriteLine("This operation will result in a mathematical error.\n");
                     }
                     else Console.WriteLine("Your result: {0:0.##}\n", result);
                 }
-                catch (Exception e)
+                catch (Exception e) //오류처리 
                 {
                     Console.WriteLine("Oh no! An exception occurred trying to do the math.\n - Details: " + e.Message);
                 }
