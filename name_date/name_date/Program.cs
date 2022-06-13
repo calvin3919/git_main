bcc67917ef6926620808b6be4519a071c4bb39f3
calvin3919 <calvin3919@gmail.com>
2022-06-13 10:50:16 -0700

    name_date

---
+++ name_date/name_date/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace webapp
+{
+
+    class program
+    {
+        static void Main(string[] agrs)
+
+
+        {
+            Console.WriteLine("What is your name?");
+            var name = Console.ReadLine();
+            DateTime currentDate = DateTime.Now;
+            Console.WriteLine($"{Environment.NewLine}Hello, {name}, on {currentDate:d} at {currentDate:t}!");
+            Console.Write($"{Environment.NewLine}Press any key to exit....");
+            Console.ReadKey(true);
+        }
+    }
+
+
+
+
+}
